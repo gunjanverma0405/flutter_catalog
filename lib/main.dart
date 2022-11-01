a8f3beca698d3f6d080cc7a8a67c24c67ec87d22
gunjanverma0405 <gunjanverma0512@gmail.com>
2022-11-01 18:21:18 +0530

    Day2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,8 +21,31 @@ class MyApp extends StatelessWidget {
     //final pi = 3.14; // data is modifiable
     //difference between const and final is for ex we can add a element in list in final but not in const
 
+    //bringvegetables(); //bringvegetables({int rupees = 100})
+    //bringvegetables(rupees: 50); //bringvegetables({int rupees = 100})
+    //bringvegetables(thaila: false);
+
     return MaterialApp(
-      home: HomePage(),
+      //home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.light, //this is overriding primaryswatch
+        //primarySwatch: Colors.red,
+      ),
+      routes: {
+        // "/" -> this means home only so remove the home otherwise this will throw error
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
+
+  // bringvegetables({required bool thaila, int rupees = 100}) {
+  //   //take cycle
+  //   //go to market
+  // }
 }
