aed366dfe4ad3a3e390dc5b6aaf8e3f09b000b0f
gunjanverma0405 <gunjanverma0512@gmail.com>
2022-11-04 19:39:56 +0530

    Day 5 Completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+//Here the _ means in class name _LoginPageState that it is private
+class _LoginPageState extends State<LoginPage> {
+  String name = " ";
+  bool changeButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,9 +29,9 @@ class LoginPage extends StatelessWidget {
               height: 15.0,
               //child: Text("Hello there"),
             ),
-            const Text(
-              "Welcome",
-              style: TextStyle(
+            Text(
+              "Welcome $name",
+              style: const TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
                 color: Colors.deepPurpleAccent,
@@ -45,6 +54,10 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter username",
                       labelText: "Username",
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -56,14 +69,55 @@ class LoginPage extends StatelessWidget {
                   const SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                    style: TextButton.styleFrom(minimumSize: Size(150, 35)),
-                    onPressed: () {
+                  //We can also use geture detector in place of Inkwell but inkwell gives good effect while gesture detector only
+                  //make in clickable
+                  InkWell(
+                    onTap: (() async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(const Duration(seconds: 1));
                       Navigator.pushNamed(context, MyRoutes.homeRoute);
-                      //print("Hi");
-                    },
-                    child: const Text("Login"),
+                    }),
+                    child: AnimatedContainer(
+                      //Giving duration is a necessity in animated container
+                      duration: const Duration(seconds: 1),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      //color: Colors.deepPurpleAccent,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurpleAccent,
+                        //we cannot give shape and borderradius together because they are a bit contrasting
+                        // shape:
+                        //     changeButton ? BoxShape.circle : BoxShape.rectangle,
+                        //When we are using decoration, we have to give color in decoration otherwise we can give in container itself
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                      ),
+                      child: changeButton
+                          ? const Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : const Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                    ),
                   ),
+                  // ElevatedButton(
+                  //   style: TextButton.styleFrom(minimumSize: Size(150, 35)),
+                  //   onPressed: () {
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //     //print("Hi");
+                  //   },
+                  //   child: const Text("Login"),
+                  // ),
                 ],
               ),
             ),
